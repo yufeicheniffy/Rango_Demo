4e4c9a1da9cd1c310e69d074f5eb383a98fe52ed
yufeicheniffy <efiechan@outlook.com>
2020-03-07 21:12:24 +0800

    Add Django model module, add admin.py and models.py( add category and page)

--- rango_Defualt/admin.py
+++ rango_Defualt/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib import admin
+from rango_Defualt.models import Category, Page
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page)
+
+
